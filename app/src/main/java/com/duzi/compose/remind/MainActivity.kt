db46b92e09a4d9ddc6eb32b7268e3ac51b94dfd9
kade <kade@daangn.com>
2023-02-26 00:29:26 +0900

    DemoSliderPreview

--- app/src/main/java/com/duzi/compose/remind/MainActivity.kt
+++ app/src/main/java/com/duzi/compose/remind/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.duzi.compose.remind.ui.theme.ComposeRemindTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +26,9 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    DemoSlider(30f) { value ->
+                        println("position : $value")
+                    }
                 }
             }
         }
@@ -39,5 +45,25 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     ComposeRemindTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun DemoSlider(sliderPosition: Float, onPositionChange : (Float) -> Unit) {
+    Slider(
+        modifier = Modifier.padding(10.dp),
+        valueRange = 20f..40f,
+        value = sliderPosition,
+        onValueChange = onPositionChange
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DemoSliderPreview() {
+    ComposeRemindTheme {
+        DemoSlider(30f) { value ->
+            println("position : $value")
+        }
     }
 }
