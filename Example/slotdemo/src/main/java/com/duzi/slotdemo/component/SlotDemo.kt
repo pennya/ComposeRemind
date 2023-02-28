7a3eae8de9c9d10604be42c12954c18b160fd4c9
kade <kade@daangn.com>
2023-03-01 02:16:35 +0900

    SlotDemo

---
+++ Example/slotdemo/src/main/java/com/duzi/slotdemo/component/SlotDemo.kt
@@ -0,0 +1,37 @@
+package com.duzi.slotdemo.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SlotDemo(
+    topContent: @Composable () -> Unit,
+    middleContent: @Composable () -> Unit,
+    bottomContent: @Composable () -> Unit
+) {
+    Column {
+        topContent()
+        middleContent()
+        bottomContent()
+    }
+}
+
+@Composable
+fun ButtonDemo() {
+    Button(onClick = {}) {
+        Text("Button")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SlotDemoPreview() {
+    SlotDemo(
+        topContent = { Text("Top") },
+        middleContent = { ButtonDemo() },
+        bottomContent = { Text("Bottom") }
+    )
+}
