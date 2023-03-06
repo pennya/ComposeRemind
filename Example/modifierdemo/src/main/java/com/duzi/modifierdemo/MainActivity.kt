9a990fa6d642f0e4112f971275a7a7437c5c00ca
kade <kade@daangn.com>
2023-03-07 00:22:12 +0900

    모디파이어 만들기

--- Example/modifierdemo/src/main/java/com/duzi/modifierdemo/MainActivity.kt
+++ Example/modifierdemo/src/main/java/com/duzi/modifierdemo/MainActivity.kt
@@ -3,14 +3,18 @@ package com.duzi.modifierdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.duzi.modifierdemo.ui.theme.ComposeRemindTheme
 
@@ -33,8 +37,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DemoScreen() {
+    val modifier = Modifier
+        .border(width = 2.dp, color = Color.Blue)
+        .padding(all = 10.dp)
+
     Text(
         text = "Hello Compose!",
+        modifier = modifier,
         fontSize = 40.sp,
         fontWeight = FontWeight.Bold
     )
