60e41559936e219ab28cef80c265d428b1f97959
kade <kade@daangn.com>
2023-03-07 00:19:52 +0900

    ModifierDemo init

---
+++ Example/modifierdemo/src/main/java/com/duzi/modifierdemo/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.duzi.modifierdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.duzi.modifierdemo.ui.theme.ComposeRemindTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeRemindTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DemoScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DemoScreen() {
+    Text(
+        text = "Hello Compose!",
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeRemindTheme {
+        DemoScreen()
+    }
+}
