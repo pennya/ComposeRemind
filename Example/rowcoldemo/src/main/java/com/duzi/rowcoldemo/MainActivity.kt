0423cfc4f6749f650cb0fd04d824de81ec22184a
kade <kade@daangn.com>
2023-03-08 02:23:30 +0900

    Row와 Modifier 사용

--- Example/rowcoldemo/src/main/java/com/duzi/rowcoldemo/MainActivity.kt
+++ Example/rowcoldemo/src/main/java/com/duzi/rowcoldemo/MainActivity.kt
@@ -3,13 +3,22 @@ package com.duzi.rowcoldemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.duzi.rowcoldemo.ui.theme.ComposeRemindTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,9 +40,42 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
+    Row {
+        TextCell(
+            text = "1",
+            modifier = Modifier
+                .padding(all = 4.dp)
+                .size(100.dp, 100.dp)
+                .border(width = 2.dp, color = Color.Red)
+        )
+        TextCell(text = "2")
+        TextCell(text = "3")
+        TextCell(text = "4")
+        TextCell(text = "5")
+        TextCell(text = "6")
+        TextCell(text = "7")
+        TextCell(text = "8")
+        TextCell(text = "9")
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun TextCell(text: String, modifier: Modifier = Modifier) {
+    val cellModifier = Modifier
+        .padding(all = 4.dp)
+        .size(100.dp, 100.dp)
+        .border(width = 4.dp, color = Color.Black)
+
+    Text(
+        text = text,
+        modifier = cellModifier.then(modifier),
+        fontSize = 70.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     ComposeRemindTheme {
