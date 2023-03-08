4932792512f1c83ece56b5503987b4e8563fd2fb
kade <kade@daangn.com>
2023-03-08 22:52:12 +0900

    Surface와 child의 width....

--- Example/rowcoldemo/src/main/java/com/duzi/rowcoldemo/component/Weight.kt
+++ Example/rowcoldemo/src/main/java/com/duzi/rowcoldemo/component/Weight.kt
@@ -1,9 +1,16 @@
 package com.duzi.rowcoldemo.component
 
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.duzi.rowcoldemo.TextCell
 
 @Composable
@@ -19,4 +26,32 @@ fun WeightComposable() {
 @Composable
 fun WeightComposablePreview() {
     WeightComposable()
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun Content() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        MainScreen()
+    }
+}
+
+@Composable
+fun MainScreen() {
+    //BoxWithConstraints
+    Box(
+        propagateMinConstraints = false,
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .width(150.dp)
+                .border(2.dp, color = Color.Black)
+        ) {
+            Text(text = "12123123")
+        }
+    }
 }
