b2b6fdedc68e2438316c3113a5b4939eab12187a
kade <kade@daangn.com>
2023-03-22 03:01:14 +0900

    ColumnList add

--- Example/listdemo/src/main/java/com/duzi/listdemo/MainActivity.kt
+++ Example/listdemo/src/main/java/com/duzi/listdemo/MainActivity.kt
@@ -3,10 +3,16 @@ package com.duzi.listdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.duzi.listdemo.ui.theme.ComposeRemindTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +25,35 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    ColumnList()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    ColumnList()
+}
+
+@Composable
+fun ColumnList() {
+    Column() {
+        repeat(500) {
+            Text(
+                text = "List Item $it",
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(5.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeRemindTheme {
+        MainScreen()
     }
 }
