37f73cb4e7b060c620f7d5404cffd2e2b3d59f45
kade <kade@daangn.com>
2023-03-22 03:06:14 +0900

    animateScrollTo

--- Example/listdemo/src/main/java/com/duzi/listdemo/MainActivity.kt
+++ Example/listdemo/src/main/java/com/duzi/listdemo/MainActivity.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.duzi.listdemo.ui.theme.ComposeRemindTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,14 +46,45 @@ fun MainScreen() {
 @Composable
 fun ColumnList() {
     val scrollState = rememberScrollState()
+    val coroutineScope = rememberCoroutineScope()
+    
+    Column {
+        Row() {
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollTo(0)
+                    }
+                },
+                modifier = Modifier
+                    .weight(0.5f)
+                    .padding(2.dp)
+            ) {
+                Text(text = "Top")
+            }
 
-    Column(modifier = Modifier.verticalScroll(scrollState)) {
-        repeat(500) {
-            Text(
-                text = "List Item $it",
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.padding(5.dp)
-            )
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollTo(scrollState.maxValue)
+                    }
+                },
+                modifier = Modifier
+                    .weight(0.5f)
+                    .padding(2.dp)
+            ) {
+                Text(text = "Bottom")
+            }
+        }
+        
+        Column(modifier = Modifier.verticalScroll(scrollState)) {
+            repeat(500) {
+                Text(
+                    text = "List Item $it",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(5.dp)
+                )
+            }
         }
     }
 }
