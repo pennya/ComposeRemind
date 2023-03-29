3053ffa2934308c13ad814fe1b912e56a60ef792
kade <kade@daangn.com>
2023-03-29 12:48:34 +0900

    AnimatedVisibility, OutlinedButton, 스크롤 추가

--- Example/lazylistdemo/src/main/java/com/duzi/lazylistdemo/MainActivity.kt
+++ Example/lazylistdemo/src/main/java/com/duzi/lazylistdemo/MainActivity.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.duzi.lazylistdemo.ui.theme.ComposeRemindTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -56,24 +57,54 @@ fun MainScreen(itemArray: Array<out String>) {
         Toast.makeText(content, text, Toast.LENGTH_SHORT).show()
     }
 
-    LazyColumn {
-        groupedItems.forEach { (manufacturer, models) ->
-            stickyHeader {
-                Text(
-                    text = manufacturer,
-                    color = Color.White,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(Color.Gray)
-                        .padding(5.dp),
-                )
-            }
+    val listState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    val displayButtonVisible = listState.firstVisibleItemIndex > 5
 
-            items(models) { model ->
-                MyListItem(
-                    item = model,
-                    onItemClick = onListItemClick
-                )
+    Box {
+        LazyColumn(
+            state = listState,
+            contentPadding = PaddingValues(bottom = 40.dp),
+        ) {
+            groupedItems.forEach { (manufacturer, models) ->
+                stickyHeader {
+                    Text(
+                        text = manufacturer,
+                        color = Color.White,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.Gray)
+                            .padding(5.dp),
+                    )
+                }
+
+                items(models) { model ->
+                    MyListItem(
+                        item = model,
+                        onItemClick = onListItemClick
+                    )
+                }
+            }
+        }
+
+        AnimatedVisibility(
+            visible = displayButtonVisible,
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            OutlinedButton(
+                onClick = {
+                    coroutineScope.launch {
+                        listState.scrollToItem(0)
+                    }
+                },
+                border = BorderStroke(1.dp, Color.Gray),
+                shape = RoundedCornerShape(50),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = Color.DarkGray
+                ),
+                modifier = Modifier.padding(5.dp)
+            ) {
+                Text(text = "Scroll to top")
             }
         }
     }
