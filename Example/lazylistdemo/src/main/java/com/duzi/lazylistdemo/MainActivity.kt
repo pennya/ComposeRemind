f5acceb335cae55479d604f1f125a59ca4e69d1c
kade <kade@daangn.com>
2023-03-29 12:21:48 +0900

    coil 라이브러리 추가 및 연동

--- Example/lazylistdemo/src/main/java/com/duzi/lazylistdemo/MainActivity.kt
+++ Example/lazylistdemo/src/main/java/com/duzi/lazylistdemo/MainActivity.kt
@@ -3,18 +3,30 @@ package com.duzi.lazylistdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.duzi.lazylistdemo.ui.theme.ComposeRemindTheme
 
 class MainActivity : ComponentActivity() {
+
+    private var itemArray: Array<String>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        itemArray = resources.getStringArray(R.array.car_array)
+
         setContent {
             ComposeRemindTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen(itemArray = itemArray!!)
                 }
             }
         }
@@ -30,14 +42,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen(itemArray: Array<out String>) {
+    ImageLoader(item = itemArray[0])
+}
+
+@Composable
+fun ImageLoader(item: String) {
+    val url = "https://www.ebookfrenzy.com/book_examples/car_logos/" +
+            item.substringBefore(" ") + "_logo.png"
+
+    Image(
+        painter = rememberAsyncImagePainter(url),
+        contentDescription = "car Image",
+        contentScale = ContentScale.Fit,
+        modifier = Modifier.size(75.dp)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    val itemArray: Array<String> = arrayOf("Test1", "Test2", "Test3")
+
     ComposeRemindTheme {
-        Greeting("Android")
+        MainScreen(itemArray = itemArray)
     }
 }
