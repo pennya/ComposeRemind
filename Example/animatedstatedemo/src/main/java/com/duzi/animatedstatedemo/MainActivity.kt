2473a2d3367b84acfcb4508ee6825d4dd15d92e9
kade <kade@daangn.com>
2023-04-05 01:26:53 +0900

    기본 UI 세팅

--- Example/animatedstatedemo/src/main/java/com/duzi/animatedstatedemo/MainActivity.kt
+++ Example/animatedstatedemo/src/main/java/com/duzi/animatedstatedemo/MainActivity.kt
@@ -3,13 +3,22 @@ package com.duzi.animatedstatedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.duzi.animatedstatedemo.ui.theme.ComposeRemindTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    RotateDemo()
                 }
             }
         }
@@ -30,14 +39,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun RotateDemo() {
+    var rotated by remember { mutableStateOf(false) }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.propeller),
+            contentDescription = "fan",
+            modifier = Modifier
+                .padding(10.dp)
+                .size(300.dp)
+        )
+
+        Button(
+            onClick = { rotated = !rotated },
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Text(text = "Rotate Propeller")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeRemindTheme {
-        Greeting("Android")
+        RotateDemo()
     }
 }
