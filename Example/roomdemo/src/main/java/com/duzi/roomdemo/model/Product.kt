abc3da79179ee4637835c6d40bc5343ca555998a
kade <kade@daangn.com>
2023-04-19 02:06:19 +0900

    entity 추가

---
+++ Example/roomdemo/src/main/java/com/duzi/roomdemo/model/Product.kt
@@ -0,0 +1,16 @@
+package com.duzi.roomdemo.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "products")
+data class Product(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "productId")
+    val id: Int = 0,
+
+    @ColumnInfo(name = "productName")
+    val productName: String = "",
+    val quantity: Int = 0
+)
