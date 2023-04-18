236031b5c35aa1e1d1fb0834508347f6f7ece995
kade <kade@daangn.com>
2023-04-19 03:27:05 +0900

    뷰모델 생성 및 주입

--- Example/roomdemo/src/main/java/com/duzi/roomdemo/MainActivity.kt
+++ Example/roomdemo/src/main/java/com/duzi/roomdemo/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.duzi.roomdemo
 
+import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.duzi.roomdemo.presentation.MainViewModel
 import com.duzi.roomdemo.ui.theme.ComposeRemindTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +38,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val owner = LocalViewModelStoreOwner.current
+                    owner?.let {
+                        val viewModel: MainViewModel = viewModel(
+                            it,
+                            "MainViewModel",
+                            MainViewModelFactory(
+                                LocalContext.current.applicationContext as Application
+                            )
+                        )
+
+                        ScreenSetup(viewModel)
+                    }
                 }
             }
         }
@@ -30,14 +57,92 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ScreenSetup(viewModel: MainViewModel) {
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeRemindTheme {
-        Greeting("Android")
+fun MainScreen(
+    viewModel: MainViewModel
+) {
+
+}
+
+@Composable
+fun TitleRow(head1: String, head2: String, head3: String) {
+    Row(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+            .fillMaxSize()
+            .padding(5.dp)
+    ) {
+        Text(
+            text = head1,
+            color = Color.White,
+            modifier = Modifier.weight(0.1f),
+        )
+        Text(
+            text = head2,
+            color = Color.White,
+            modifier = Modifier.weight(0.2f),
+        )
+        Text(
+            text = head3,
+            color = Color.White,
+            modifier = Modifier.weight(0.2f),
+        )
+
+    }
+}
+
+@Composable
+fun ProductRow(id: Int, name: String, quantity: Int) {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp)
+    ) {
+        Text(
+            id.toString(),
+            modifier = Modifier.weight(0.1f),
+        )
+        Text(
+            name,
+            modifier = Modifier.weight(0.2f),
+        )
+        Text(
+            quantity.toString(),
+            modifier = Modifier.weight(0.2f),
+        )
+    }
+}
+
+@Composable
+fun CustomTextField(
+    title: String,
+    textState: String,
+    onTextChange: (String) -> Unit,
+    keyboardType: KeyboardType
+) {
+    OutlinedTextField(
+        value = textState,
+        onValueChange = { onTextChange(it) },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType
+        ),
+        singleLine = true,
+        label = { Text(text = title) },
+        modifier = Modifier.padding(10.dp),
+        textStyle = androidx.compose.ui.text.TextStyle(
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp
+        )
+    )
+}
+
+// TODO hilt migration
+class MainViewModelFactory(private val application: Application): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return MainViewModel(application) as T
     }
 }
