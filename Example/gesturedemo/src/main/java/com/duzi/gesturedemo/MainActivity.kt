679ac3a4c5b03a12e37670c81ec65d95bf7bbf5b
kade <kade@daangn.com>
2023-04-25 22:44:27 +0900

    detectDragGestures, draggable 테스트

--- Example/gesturedemo/src/main/java/com/duzi/gesturedemo/MainActivity.kt
+++ Example/gesturedemo/src/main/java/com/duzi/gesturedemo/MainActivity.kt
@@ -4,8 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,11 +12,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.duzi.gesturedemo.ui.theme.ComposeRemindTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,12 +33,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ClickDemo()
+                    DragDemo()
                 }
             }
         }
     }
 }
+
+@Composable
+fun Dp.dpToPx() = with(LocalDensity.current) { this@dpToPx.toPx() }
+
+
+@Composable
+fun Int.pxToDp() = with(LocalDensity.current) { this@pxToDp.toDp() }
 
 @Composable
 fun ClickDemo() {
@@ -89,10 +100,58 @@ fun ClickDemo() {
     }
 }
 
+@Composable
+fun DragDemo() {
+    BoxWithConstraints(
+        modifier = Modifier
+            .fillMaxSize()
+            .clipToBounds()
+    ) {
+        var xOffset by remember { mutableStateOf(0f) }
+        val parentWidth = constraints.maxWidth
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            val boxSize = 100.dp
+
+            Box(
+                Modifier
+                    .offset { IntOffset(xOffset.roundToInt(), 0) }
+                    .size(boxSize)
+                    .background(Color.Blue)
+                    .align(Alignment.Start)
+                    .pointerInput(Unit) {
+                        val boxSize = this.size
+                        detectDragGestures { change, dragAmount ->
+                            xOffset = (xOffset + dragAmount.x).coerceIn(
+                                0f,
+                                parentWidth.toFloat() - boxSize.width.toFloat()
+                            )
+                        }
+                    }
+                    /*.draggable(
+                        orientation = Orientation.Horizontal,
+                        state = rememberDraggableState { delta ->
+                            xOffset = (xOffset + delta).coerceIn(
+                                0f,
+                                parentWidth.toFloat()
+                            )
+                        }
+                    )*/
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+            Text("xOffset: $xOffset")
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeRemindTheme {
-        ClickDemo()
+        DragDemo()
     }
 }
